c0f164e332b5cae804f019494dda1c621b8c860c
cristofer <crissttofer2021@gmail.com>
2023-01-28 10:49:59 -0500

    Fin proyecto

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Proyecto1{
+    class Program{
+        public static void Main(string[] args){
+        }
+    }
+}
